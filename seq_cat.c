93308e8c8e9641286ee2a6b1bcbb61f7eeab6536
Jonathan Taylor <jtaylor@datafoundry.com>
2021-06-02 09:09:47 -0500

    Load list of longs into data structure

--- seq_cat.c
+++ seq_cat.c
@@ -9,8 +9,14 @@ int main() {
   long int the_actual_number;
   long int the_long_total = 0;
   int total_lines_read = 0;
+	long int *list_of_longs;
+	long int *current_long;
+	int i;
 
   const unsigned char* name = "advent-20-01,s,r";
+
+	list_of_longs = (long int*)malloc(200 * sizeof(long int));
+
   printf("'%s'", name);
   lfn = 1;
 
@@ -28,6 +34,7 @@ int main() {
 
   number_string[0] = 0;
   string_position = 0;
+	current_long = list_of_longs;
   do {
     
     bytes_read = cbm_read( lfn, &data, 1);
@@ -38,10 +45,12 @@ int main() {
           number_string[string_position] = 0;
           // printf("%s\n", number_string);
           the_actual_number = strtol(number_string, endptr, 0);
+					*current_long = the_actual_number;
+					current_long += sizeof(long int);
 
           the_long_total += the_actual_number;
           total_lines_read++;
-          printf("%ld\n", the_actual_number);
+          //printf("%ld\n", the_actual_number);
 
           string_position = 0;
       }
@@ -53,6 +62,11 @@ int main() {
 
   }
   while ( bytes_read > 0 );
+
+	for(i=0; i < total_lines_read * sizeof(long int); i = i + sizeof(long int) ) {
+		printf("%ld\n", *(list_of_longs + i) );
+	}
+	
 
   cbm_k_close( lfn );
 
