6847a82c34f03f185c5492296a3fb97d50aa526c
Jonathan W. Taylor <elderling@gmail.com>
2021-06-20 09:36:11 -0500

    Extract clunky read-on-line function

--- advent_02b.c
+++ advent_02b.c
@@ -3,20 +3,12 @@
 #include <cbm.h>
 #include <string.h>
 
-unsigned char is_char_at_index( unsigned char the_char, unsigned char index, unsigned char the_string[] );
+void gimme_a_line( char lfn, char *put_it_here, char *how_many );
 
-unsigned char is_char_at_index( unsigned char the_char, unsigned char index, unsigned char the_string[] ) {
-  if ( the_string[index] == the_char ) {
-    return 1;
-  }
+char aoc_open_file(const char name[], unsigned int lfn);
 
-  return 0;
-}
-
-unsigned char aoc_open_file(const unsigned char name[], unsigned int lfn);
-
-unsigned char aoc_open_file(const unsigned char name[], unsigned int lfn) {
-  unsigned char open_result,chkin_result;
+char aoc_open_file(const char name[], unsigned int lfn) {
+ char open_result,chkin_result;
 
   open_result = cbm_open( lfn, 8, 0, name );
 
@@ -32,74 +24,60 @@ unsigned char aoc_open_file(const unsigned char name[], unsigned int lfn) {
   return 0;
 }
 
-int main() {
-  unsigned char string_position, lfn, bytes_read, data, result;
-  unsigned char the_string[32];
-  int total_lines_read = 0;
-  int total_valid_passwords = 0;
-  signed char min, max, count;
-  unsigned char *min_str, *max_str, *the_range_str, *the_letter_str, *the_password_str, the_letter;
+void gimme_a_line( char lfn, char *put_it_here, char *how_many ) {
+   char data, bytes_read;
+   char string_position = 0;
+   *how_many = 0;
+   put_it_here[0] = 0;
 
-  const unsigned char* name = "advent-20-02,s,r";
-
-  lfn = 1;
-
-  result = aoc_open_file( name, lfn );
-
-  count = 0;
-  the_string[0] = 0;
-  string_position = 0;
   do {
     
     bytes_read = cbm_read( lfn, &data, 1);
 
     if ( bytes_read ) {
       if ( data == 0x0d ) {
-          the_string[string_position] = 0;
+          put_it_here[string_position] = 0;
 
-          the_range_str = strtok(the_string, " ");
-          //printf("Range: %s\n", the_range_str);
-          the_letter_str = strtok(NULL, " ");
-          the_password_str = strtok(NULL, " ");
-
-          min_str = strtok(the_range_str, "-");
-          max_str = strtok(NULL, "-");
-
-          min = atoi(min_str);
-          max = atoi(max_str);
-          printf("range: %d-%d\n", min, max);
-          the_letter = the_letter_str[0];
-
-          //printf("password: %s\n", the_password_str);
-          printf("letter %c appears in %s %d times", the_letter, the_password_str, count);
-          printf("\n");
-          if ( count >= min && count <= max ) {
-            total_valid_passwords++;
-          }
-
-          total_lines_read++;
-          //printf("%s\n", the_string);
-          //printf("min is %s\n", min_str);
-          //printf("the_rest is %s\n", the_rest_str);
-
-          //max_str = strtok(the_rest_str, " ");
-
-          //the_rest_str =  strtok(NULL, "-");
-
-
+          *how_many = string_position + 1;
+          return;
           string_position = 0;
       }
       else {
-        the_string[string_position++] = data;
+        put_it_here[string_position++] = data;
+        *how_many = string_position + 1;
       }
     }
 
   }
   while ( bytes_read > 0 );
 
+  return;
+}
+
+int main() {
+  char lfn, result;
+  char the_string[32];
+  int total_lines_read = 0;
+  int total_valid_passwords = 0;
+  char how_many;
+
+  const char* name = "advent-20-02,s,r";
+
+  lfn = 1;
+
+  result = aoc_open_file( name, lfn );
+
+  do {
+    gimme_a_line( lfn, the_string, &how_many );
+    if (strlen(the_string) > 0) {
+      total_lines_read++;
+      printf( "%s\n", the_string );
+    }
+  }
+  while ( strlen(the_string) > 0 );
+
   cbm_k_close( lfn );
 
-  printf("\n");
   printf("Total lines read = %d", total_lines_read);
   printf("Total valid passwords = %d", total_valid_passwords);
 
